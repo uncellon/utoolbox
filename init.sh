800cbf535077e642b33fd3adc02dae3362223e44
Dmitry Plastinin <uncellon@yandex.ru>
2022-06-06 10:36:51 +0300

    Core updated and init script added

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR/core" && git checkout master && git pull
+cd "$SCRIPT_DIR/gpio" && git checkout master && git pull
+cd "$SCRIPT_DIR/serialport" && git checkout master && git pull
+cd "$SCRIPT_DIR/timers" && git checkout master && git pull
