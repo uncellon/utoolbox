e029c91948ac840508c327db61b49892194271ab
Dmitry Plastinin <uncellon@yandex.ru>
2023-04-25 18:38:13 +0300

    Add IPC

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ cd "$SCRIPT_DIR/core" && git checkout master && git pull
 cd "$SCRIPT_DIR/gpio" && git checkout master && git pull
 cd "$SCRIPT_DIR/serialport" && git checkout master && git pull
 cd "$SCRIPT_DIR/timers" && git checkout master && git pull
+cd "$SCRIPT_DIR/ipc" && git checkout master && git pull
