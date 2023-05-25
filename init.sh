a0d0864b7aa9c9833a4d3280f82455d8e76daee3
Dmitry Plastinin <uncellon@yandex.ru>
2023-05-25 22:45:05 +0300

    Sync 2023-05-25

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR/core" && git checkout master && git pull
+cd "$SCRIPT_DIR/timers" && git checkout master && git pull
 cd "$SCRIPT_DIR/gpio" && git checkout master && git pull
 cd "$SCRIPT_DIR/serialport" && git checkout master && git pull
-cd "$SCRIPT_DIR/timers" && git checkout master && git pull
-cd "$SCRIPT_DIR/ipc" && git checkout master && git pull
+cd "$SCRIPT_DIR/logger" && git checkout master && git pull
